b5e19902ef9d42fc8bf2b4a2f41e7f503116b8ba
DHLAP\h <vdavidhammond@gmail.com>
2017-07-27 15:55:51 -0600

    Added in additional work info, formatted text so that it does not wrap with photo, Added in a contact button that pops outa div where I can add direct ways to contact me

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
 $(document).ready(function () {
+  $(".contactClick").click(()=>{
+      let dataVal = $(".contactClick").attr("data-active");
+      console.log(dataVal);
+        if (dataVal === "closed") {
+          $("#switchHere").removeClass("glyphicon-chevron-right");
+          $("#switchHere").addClass("glyphicon-chevron-left");
+        $(".contactClick").attr("data-active", "open");
+        $("#contactInfo").toggle(function () {
+        $(this).parent().animate({right:'0px'}, {queue: false, duration: 500});
+        }, function () {
+        $(this).parent().animate({right:'-280px'}, {queue: false, duration: 500});  
+        });
+        }
+        else {
+        $(".contactClick").attr("data-active", "closed");
+        $("#contactInfo").toggle(function () {
+        $(this).parent().animate({left:'0px'}, {queue: false, duration: 500});
+        }, function () {
+        $(this).parent().animate({left:'-280px'}, {queue: false, duration: 500});
+        });
+        $("#switchHere").removeClass("glyphicon-chevron-left");
+        $("#switchHere").addClass("glyphicon-chevron-right");
+        }
+
+  });
+  // $("#emailFormat").click(()=>{
+  //   document.location = "mailto:vdavidhammond@gmail.com";
+  // });
 $(".textAlign").click(function () {
   let dataVal = $(this).attr("data-tag");
   if (dataVal === "objective") {
     $("#background").addClass("hide");
     $("#portImages").addClass("hide");
     $("#contactPort").addClass("hide");
+    $("#education").addClass("hide");
     $("#objective").removeClass("hide");
 
     // ad din portfolio stuff
@@ -13,6 +42,7 @@ $(".textAlign").click(function () {
     $("#objective").addClass("hide");
     $("#portImages").addClass("hide");
     $("#contactPort").addClass("hide");
+    $("#education").addClass("hide");
     $("#background").removeClass("hide");
   }
   else if (dataVal === "portfolio") {
@@ -21,6 +51,13 @@ $(".textAlign").click(function () {
     $("#bottomSide").addClass("hide");
     $("#portImages").removeClass("hide");
     $("#contactPort").removeClass("hide");
+  }
+  else if (dataVal === "education") {
+    $("#objective").addClass("hide");
+    $("#portImages").addClass("hide");
+    $("#contactPort").addClass("hide");
+    $("#background").addClass("hide");
+    $("#education").removeClass("hide");
   }
   else {
     console.log("A Fatal Error has occurred");
