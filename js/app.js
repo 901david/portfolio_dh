f77aff04eb6e523c802df2114c451086d8e53c8b
DHLAP\h <vdavidhammond@gmail.com>
2017-07-27 16:02:03 -0600

    If you click my email address now it opens contact bar as well.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,35 @@
+function contactBar () {
+  let dataVal = $(".contactClick").attr("data-active");
+  console.log(dataVal);
+    if (dataVal === "closed") {
+      $("#switchHere").removeClass("glyphicon-chevron-right");
+      $("#switchHere").addClass("glyphicon-chevron-left");
+    $(".contactClick").attr("data-active", "open");
+    $("#contactInfo").toggle(function () {
+    $(this).parent().animate({right:'0px'}, {queue: false, duration: 500});
+    }, function () {
+    $(this).parent().animate({right:'-280px'}, {queue: false, duration: 500});
+    });
+    }
+    else {
+    $(".contactClick").attr("data-active", "closed");
+    $("#contactInfo").toggle(function () {
+    $(this).parent().animate({left:'0px'}, {queue: false, duration: 500});
+    }, function () {
+    $(this).parent().animate({left:'-280px'}, {queue: false, duration: 500});
+    });
+    $("#switchHere").removeClass("glyphicon-chevron-left");
+    $("#switchHere").addClass("glyphicon-chevron-right");
+    }
+};
 $(document).ready(function () {
   $(".contactClick").click(()=>{
-      let dataVal = $(".contactClick").attr("data-active");
-      console.log(dataVal);
-        if (dataVal === "closed") {
-          $("#switchHere").removeClass("glyphicon-chevron-right");
-          $("#switchHere").addClass("glyphicon-chevron-left");
-        $(".contactClick").attr("data-active", "open");
-        $("#contactInfo").toggle(function () {
-        $(this).parent().animate({right:'0px'}, {queue: false, duration: 500});
-        }, function () {
-        $(this).parent().animate({right:'-280px'}, {queue: false, duration: 500});  
-        });
-        }
-        else {
-        $(".contactClick").attr("data-active", "closed");
-        $("#contactInfo").toggle(function () {
-        $(this).parent().animate({left:'0px'}, {queue: false, duration: 500});
-        }, function () {
-        $(this).parent().animate({left:'-280px'}, {queue: false, duration: 500});
-        });
-        $("#switchHere").removeClass("glyphicon-chevron-left");
-        $("#switchHere").addClass("glyphicon-chevron-right");
-        }
+    contactBar();
 
   });
-  // $("#emailFormat").click(()=>{
-  //   document.location = "mailto:vdavidhammond@gmail.com";
-  // });
+  $("#emailFormat").click(()=>{
+    contactBar();
+  });
 $(".textAlign").click(function () {
   let dataVal = $(this).attr("data-tag");
   if (dataVal === "objective") {
